4a9ad37156aa84dec799c2578b28fde712718350
bas <tima.bashirov92@gmail.com>
2026-02-10 15:08:20 +0300

    fix: correct JSON tag formatting

--- back/db/db.go
+++ back/db/db.go
@@ -13,7 +13,7 @@ import (
 
 type Task struct {
 	ID      string `json:"id"`
-	Date    string `json:date`
+	Date    string `json:"date"`
 	Title   string `json:"title"`
 	Comment string `json:"comment"`
 	Repeat  string `json:"repeat"`
